d916e29ee8ba59485f800d04d0314825ee4d98d1
goodmorningnelson <mtr.ruehr@gmail.com>
2019-01-20 20:11:27 +0100

    and so it begins..

---
+++ morning-kingdom-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Cover from './components/Cover/Cover';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Cover/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ morning-kingdom-app/src/components/Cover/Cover.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cover.scss';
+
+const cover = () => {
+    return (
+    <div className="coverArea">
+        <div className="coverTitle">Happy New Year!</div>
+    </div>)
+};
+
+export default cover;
